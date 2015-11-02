c271272c3374bec4e3ab98a5b86e3f194f6c24f7
Sascha Ziemann <ceving@gmail.com>
2015-11-02 12:35:38 +0100

    Removes test functions.

--- demo.js
+++ demo.js
@@ -23,6 +23,7 @@ function dbval (entity, id, attribute, value)
 
 var peter;
 var home;
+var xy;
 
 $ (function () {
   demo.query (
@@ -44,49 +45,5 @@ $ (function () {
   home = new demo.address({street: "Second to the right",
                            locality: "Straight on till morning",
                            country: "Neverland"});
+  xy = new demo.person({firstname: "X", lastname: "Y"});
 });
-
-function test1 () {
-    var s = {};
-    var a = {n:1, m:2};
-    var i;
-    
-    for (i in a) {
-      (function(i) {
-        s[i] = function() { this[i] = a[i]; };
-        console.log (s[i]);
-      })(i);
-    }
-    
-    var x = new s.n;
-    var y = new s.m;
-    
-    console.log (x);
-    console.log (y);
-}
-
-function test2 () {
-    x = {};
-    for (i of ['a', 'b']) {
-      (function(i) {
-        x[i] = function() { this.v = i; }
-      })(i);
-    }
-
-    y = {};
-    for (i of ['a', 'b']) {
-      y[i] = function() { this.v = i; }
-    }
-}
-
-
-var S = function(es)
-{
-  for (let e of es) {
-    this[e] = function() {
-      this.v = e;
-    }
-  }
-}
-
-var s = new S(['a', 'b']);
